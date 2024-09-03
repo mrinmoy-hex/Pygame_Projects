acd49deb076b8f1bc7f0d15488cbf6ffd947c63d
mrinmoy-hex <ddas38192@gmail.com>
2024-09-03 20:26:55 +0530

    Added vector mechanics and fixed independent of fps by delta time

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 pygame.init()
 WIDTH = 1280
 HEIGHT = 720
+clock = pygame.time.Clock() # used for setting frame rate
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
@@ -18,7 +19,9 @@ x = 100
 # importing an image
 player = pygame.image.load("images/player.png").convert_alpha()
 player_rect = player.get_frect(center = (WIDTH / 2, HEIGHT /2))
-player_dir = 1
+player_dir = pygame.math.Vector2(1, -1)
+# print(player_dir)
+player_speed = 300
 
 stars = pygame.image.load(join("images", "star.png")).convert_alpha()
 
@@ -31,6 +34,7 @@ lazer_rect = lazer.get_frect(bottomleft = (20, HEIGHT - 20))
 star_pos = [(random.randint(2, WIDTH - 23), random.randint(2, HEIGHT - 23)) for _ in range(20)] 
 
 while True:
+    dt = clock.tick(120) / 1000    # delta time
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -42,17 +46,20 @@ while True:
     # draw stars
     for pos in star_pos:        
         screen.blit(stars, pos) 
-    
-            
+                
     screen.blit(player, player_rect)
-    player_rect.x += player_dir * 0.6
+    # player_rect.x += 50
+    # player_rect.y -= 50
+
+    if player_rect.top < 0 or player_rect.bottom > HEIGHT:
+        player_dir.y *= -1
     if player_rect.right > WIDTH or player_rect.left < 0:
-        player_dir *= -1
-        
-        
+        player_dir.x *= -1
+    
+    player_rect.center += player_dir * player_speed * dt
     screen.blit(lazer, lazer_rect)
     screen.blit(meteor, meteor_rect)
     
     pygame.display.update()
-    
+
 
