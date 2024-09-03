8784aa4fc2099a14722667c5e42e36623f35bf51
mrinmoy-hex <ddas38192@gmail.com>
2024-09-03 21:19:04 +0530

    Added input movements

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ x = 100
 # importing an image
 player = pygame.image.load("images/player.png").convert_alpha()
 player_rect = player.get_frect(center = (WIDTH / 2, HEIGHT /2))
-player_dir = pygame.math.Vector2(1, -1)
+player_dir = pygame.math.Vector2()
 # print(player_dir)
 player_speed = 300
 
@@ -39,24 +39,37 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        # if event.type == pygame.KEYDOWN:
+        #     print(event.key == pygame.K_0)
+            
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
 
+    # input
+    # print(pygame.mouse.get_pressed())
+    keys = pygame.key.get_pressed()
+    player_dir.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    # if keys[pygame.K_RIGHT]:
+    #     player_dir.x = 1
+    # else:
+    #     player_dir.x = 0
+    player_dir = player_dir.normalize() if player_dir else player_dir
+    player_dir.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])    
+    player_rect.center += player_dir * player_speed * dt
+    # for debugging player direction
+    print(player_dir)    
     # draw the game
     screen.fill('darkgray')
     
     # draw stars
-    for pos in star_pos:        
+    for pos in star_pos:       
         screen.blit(stars, pos) 
                 
     screen.blit(player, player_rect)
     # player_rect.x += 50
     # player_rect.y -= 50
 
-    if player_rect.top < 0 or player_rect.bottom > HEIGHT:
-        player_dir.y *= -1
-    if player_rect.right > WIDTH or player_rect.left < 0:
-        player_dir.x *= -1
-    
-    player_rect.center += player_dir * player_speed * dt
+    # player_rect.center += player_dir * player_speed * dt
     screen.blit(lazer, lazer_rect)
     screen.blit(meteor, meteor_rect)
     
