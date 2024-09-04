2b687c828b6bb45d8133d849b1c925f6af629a1e
mrinmoy-hex <ddas38192@gmail.com>
2024-09-04 19:58:54 +0530

    Organised the code with classes

--- space_game/main.py
+++ space_game/main.py
@@ -3,6 +3,34 @@ import sys
 from os.path import join
 import random
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups) -> None:
+        super().__init__(groups)
+        self.image = pygame.image.load("space_game/images/player.png").convert_alpha()
+        self.rect = self.image .get_frect(center = (WIDTH / 2, HEIGHT /2))
+        self.player_dir = pygame.math.Vector2()
+        self.player_speed = 300
+        
+    def update(self, dt) -> None:
+        keys = pygame.key.get_pressed()
+        self.player_dir.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.player_dir.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.player_dir = self.player_dir.normalize() if self.player_dir else self.player_dir
+        self.rect.center += self.player_dir * self.player_speed * dt
+        
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print("fire laser")
+
+    # for debugging player direction
+    # print(player_dir) 
+
+class Stars(pygame.sprite.Sprite):
+    def __init__(self, groups, surf) -> None:
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (random.randint(2, WIDTH - 23), random.randint(2, HEIGHT - 23)))
+
 # general setup
 pygame.init()
 WIDTH = 1280
@@ -12,26 +40,19 @@ clock = pygame.time.Clock() # used for setting frame rate
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
 
-surf = pygame.Surface((100, 200))
-surf.fill('orange')
-x = 100
+all_sprites = pygame.sprite.Group()
+# all_sprites.add(player) # not required as it's added in the group by init method
+star_surf = pygame.image.load(join("space_game","images", "star.png")).convert_alpha()
+for i in range(20):
+    Stars(all_sprites, star_surf)
+player = Player(all_sprites)
 
-# importing an image
-player = pygame.image.load("images/player.png").convert_alpha()
-player_rect = player.get_frect(center = (WIDTH / 2, HEIGHT /2))
-player_dir = pygame.math.Vector2()
-# print(player_dir)
-player_speed = 300
-
-stars = pygame.image.load(join("images", "star.png")).convert_alpha()
-
-meteor = pygame.image.load("images/meteor.png").convert_alpha()
+meteor = pygame.image.load("space_game/images/meteor.png").convert_alpha()
 meteor_rect = meteor.get_frect(center = (WIDTH / 2, HEIGHT /2))
 
-lazer = pygame.image.load("images/laser.png").convert_alpha()
+lazer = pygame.image.load("space_game/images/laser.png").convert_alpha()
 lazer_rect = lazer.get_frect(bottomleft = (20, HEIGHT - 20))
 
-star_pos = [(random.randint(2, WIDTH - 23), random.randint(2, HEIGHT - 23)) for _ in range(20)] 
 
 while True:
     dt = clock.tick(120) / 1000    # delta time
@@ -39,39 +60,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        # if event.type == pygame.KEYDOWN:
-        #     print(event.key == pygame.K_0)
             
-        # if event.type == pygame.MOUSEMOTION:
-        #     player_rect.center = event.pos
-
-    # input
-    # print(pygame.mouse.get_pressed())
-    keys = pygame.key.get_pressed()
-    player_dir.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    # if keys[pygame.K_RIGHT]:
-    #     player_dir.x = 1
-    # else:
-    #     player_dir.x = 0
-    player_dir = player_dir.normalize() if player_dir else player_dir
-    player_dir.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])    
-    player_rect.center += player_dir * player_speed * dt
-    # for debugging player direction
-    print(player_dir)    
-    # draw the game
+    all_sprites.update(dt)
+       
     screen.fill('darkgray')
-    
-    # draw stars
-    for pos in star_pos:       
-        screen.blit(stars, pos) 
-                
-    screen.blit(player, player_rect)
-    # player_rect.x += 50
-    # player_rect.y -= 50
-
-    # player_rect.center += player_dir * player_speed * dt
-    screen.blit(lazer, lazer_rect)
-    screen.blit(meteor, meteor_rect)
+    # screen.blit(player.image, player.rect) Not an ideal approach
+    all_sprites.draw(screen)
     
     pygame.display.update()
 
