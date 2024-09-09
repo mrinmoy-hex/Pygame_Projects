390479498b8763d9dc55eaa862b3b4918dc17d3f
mrinmoy-hex <ddas38192@gmail.com>
2024-09-10 01:05:55 +0530

    fixed assets path

--- startup.sh
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Added files"
-git branch -M main
-git remote add origin https://github.com/mrinmoy-hex/Pygame_Projects.git
-git push -u origin main
-echo "Process exited with success - 225 "
